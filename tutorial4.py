c8579eec5a52727d75e77a05f1aa9fb4076c2155
voidJeff <qy120838@126.com>
2015-10-07 13:58:32 -0400

    Updated from Brython Server: October 7, 2015 1:58:32 PM EDT

--- tutorial4.py
+++ tutorial4.py
@@ -3,6 +3,25 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
+class SpaceShip(Sprite):
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.01
+        self.thrust = 0
+        self.thrustframe = 1
+        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        
 class SpaceGame(App):
     def __init__(self, width, height):
         super().__init__(width, height)
@@ -14,11 +33,9 @@ class SpaceGame(App):
         SpaceShip((150,150))
         SpaceShip((200,50))
         
-class SpaceShip(Sprite):
-    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
-        Frame(227,0,292-227,125), 4, 'vertical')
-
-    def __init__(self, position):
-        super().__init__(SpaceShip.asset, position)   
+    def step(self):
+        for ship in self.getSpritesbyClass(SpaceShip):
+            ship.step()
+        
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
