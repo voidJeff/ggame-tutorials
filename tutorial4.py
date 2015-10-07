2cd94aeee69ab9530354147f047f76d88b29b7f9
voidJeff <qy120838@126.com>
2015-10-07 13:32:35 -0400

    Updated from Brython Server: October 7, 2015 1:32:35 PM EDT

--- tutorial4.py
+++ tutorial4.py
@@ -1,1 +1,10 @@
+class Circle(object):
+    def __init__(self, radius):
+        self.r = radius
+    
+    def area(self):
+        return 3.14 * self.r * self.r
 
+circle = Circle(2)
+print(circle.r)
+print(circle.area())
