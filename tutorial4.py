314c3e90d34992988283f5ebe4062379ca4bd5a7
voidJeff <qy120838@126.com>
2015-10-08 14:57:13 -0400

    Updated from Brython Server: October 8, 2015 2:57:13 PM EDT

--- tutorial4.py
+++ tutorial4.py
@@ -7,7 +7,7 @@ class SpaceShip(Sprite):
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
         Frame(227,0,292-227,125), 4, 'vertical')
 
-    def __init__(self, position, angle):
+    def __init__(self, position):
         super().__init__(SpaceShip.asset, position)
         self.vx = 0
         self.vy = 0
@@ -21,7 +21,6 @@ class SpaceShip(Sprite):
         SpaceGame.listenKeyEvent("keydown", "right arrow", self.rotateRight)
         SpaceGame.listenKeyEvent("keyup", "right arrow", self.rrOff)
         self.fxcenter = self.fycenter = 0.5
-        self.angle = angle = 0
         
     def step(self):
         self.x -= self.vx
@@ -63,10 +62,11 @@ class SpaceGame(App):
         black = Color(0, 1)
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
-        bg = Sprite(bg_asset, (0,0))
-        SpaceShip((100,100))
-        SpaceShip((150,150))
-        SpaceShip((200,50))
+        bg = Sprite(bg_asset, (0,0,0))
+        SpaceShip((100,100,0))
+        SpaceShip((150,150,0))
+        SpaceShip((200,50,0))
+        self.angle = angle
         
     def step(self):
         for ship in self.getSpritesbyClass(SpaceShip):
